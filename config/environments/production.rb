5f27878bc86b0ac3724bcbfa960c88535db79a25
Sandra Tooley <sandratooley89@gmail.com>
2018-06-25 16:45:45 -0600

    fixing email issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,10 +94,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mailgun.org',
+    address: 'smtp.gmail.com',
     port:                     587,
-    domain:               ENV["MAILGUN_DOMAIN_URL"],
-    user_name:            ENV["MAILGUN_DEFAULT_SMTP"],
-    password:             ENV["MAILGUN_PASSWORD"],
+    domain:               "gmail.com",
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain' }
 end
